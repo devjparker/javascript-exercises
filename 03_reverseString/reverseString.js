e5f33483da345daa53f33d396a5954e7bdf95556
devjparker <devinjparker@outlook.com>
2022-06-10 12:14:01 -0500

    Finish javascript exercise 3 reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,10 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let baseString = string;
+    let finalString = '';
+    for (let i = baseString.length - 1; i >= 0; i--) {
+        finalString += string[i];
+    }
+    return finalString;
 };
 
 // Do not edit below this line
