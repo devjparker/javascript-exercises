390c6a4ca0eb72a583310ab78f79d5233b6a7f87
devjparker <devinjparker@outlook.com>
2022-06-10 12:01:19 -0500

    Finish exercise 2 repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) {
+        return 'ERROR';
+    }
+    let stringToBePrinted = string;
+    let finalString = '';
+    let numTimesPrinted = num;
+    for (let i = 0; i < numTimesPrinted; i++) {
+        finalString += stringToBePrinted;
+        //return finalString;
+    }
+    return finalString;
 };
 
 // Do not edit below this line
